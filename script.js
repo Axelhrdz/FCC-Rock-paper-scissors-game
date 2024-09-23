8314d9f7280ce8b57f041b5dab4748546a82eba3
Axelhrdz <axelhernandezperez23@gmail.com>
2024-09-23 17:54:26 -0600

    starting the project

---
+++ script.js
@@ -0,0 +1,9 @@
+// Getting variables
+const rulesHeader = document.querySelector('.rulesHeader');
+const rulesContainer = document.querySelector('.rulesContainer');
+
+const dropdownToggle = () => {
+    console.log('clicking');
+
+    rulesContainer.classList.toggle('active');
+}
